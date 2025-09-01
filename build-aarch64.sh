c3d2644301e1ea9510613ab8257d6c69ad756497
bubblepipe42 <bubblepipe42@gmail.com>
2025-09-01 14:05:03 +0800

    add aarch64

---
+++ build-aarch64.sh
@@ -0,0 +1,2 @@
+docker buildx build --platform linux/arm64 -t bubblepipe42/numbat:aarch64 -f Dockerfile.aarch64 . 
+
---
+++ build-amd64.sh
@@ -0,0 +1,2 @@
+docker buildx build --platform linux/amd64 -t bubblepipe42/numbat:amd64 -f Dockerfile . 
+
